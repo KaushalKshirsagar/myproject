452c83220a615fa9176198be74f02b750d04c9a1
kaush <kaush@123.lab>
2025-05-02 06:01:43 +0530

    Changes done. You can folk!

--- file1.py
+++ file1.py
@@ -2,6 +2,4 @@ n1 = int(input("Enter number1: "))
 n2 = int(input("Enter number2: "))
 print(f"Addition of {n1} + {n2} = {n1+n2}")
 # The code of Addition of two numbers is ready
-print(f"Addition of 10 + 20 = {10+20}")
-# The code is ready
 # You can folk my code..!
